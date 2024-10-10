f4ea36524b6fd0ce6196129314679c07d29c3515
Anna Julia Curcio Marques <134973253+annaju0405@users.noreply.github.com>
2024-10-10 10:43:03 -0300

    Create ProvaFafifo.java

---
+++ ProvaFafifo.java
@@ -0,0 +1,10 @@
+public class ProvaFafifo extends ProvaUniversidade {
+    public ProvaFafifo(double av1, double av2) {
+        super(av1, av2);
+    }
+
+    public boolean aprovado() {
+        return calcularMedia() >= 6;
+    }
+}
+
